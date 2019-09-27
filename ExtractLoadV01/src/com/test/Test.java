425b725e201b002f81e40518adce8f0939760fdc
YuxuanLee <13622033275@163.com>
2019-09-27 13:41:18 +0800

    20190927 修改

--- ExtractLoadV01/src/com/test/Test.java
+++ ExtractLoadV01/src/com/test/Test.java
@@ -1,12 +1,50 @@
 package com.test;
 
+
+import java.security.Provider.Service;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.model.Util_DBase;
+
+import net.sf.json.JSONObject;
 
 public class Test {
 	public static void main(String[] args) {
-		Util_DBase base=new Util_DBase();
+//		Util_DBase base=new Util_DBase();
+//		
+//		boolean flag=base.checkDate("2019-1", "yyyy");
+//		System.out.println(flag);
+//		System.out.println("Hello World");
 		
-		boolean flag=base.checkDate("2019-1", "yyyy");
-		System.out.println(flag);
+//		Service service=new Service(provider, type, algorithm, className, aliases, attributes);
+		
+		
+		
 	}
+	
+	
+//	public List<HourTankData> getHourTankData(String Nodeno,String Oilcan){
+//		List<HourTankData> lst    		= new ArrayList<HourTankData>();
+//		
+//		try {
+//			ChecktankLocator   service		= new ChecktankLocator();
+//			ChecktankPortType  serviceSoap	= service.getchecktankHttpSoap11Endpoint();
+//			String 			   stringJson	= serviceSoap.CIF_OR_ID004(Nodeno,Oilcan);
+//			JSONObject 		   jsonObject	= new JSONObject(stringJson);
+//			
+//			System.out.println("json="+jsonObject.toString());
+////			if(jsonObject.getInt("total")==0) {
+////				System.out.println("aaaabbbccd");
+////				return lst;
+////			}
+//			
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//		}
+//		
+//		
+//		return lst;
+//	}
+	
 }
