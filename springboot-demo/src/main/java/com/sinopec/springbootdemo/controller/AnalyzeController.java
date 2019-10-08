9f303f069d03ff816c57738926f97c0a485e0320
zyyf <zyyf@tju.edu.cn>
2019-10-08 19:02:41 +0800

    20191008 添加了新的页面的文件（未测试）

---
+++ springboot-demo/src/main/java/com/sinopec/springbootdemo/controller/AnalyzeController.java
@@ -0,0 +1,24 @@
+package com.sinopec.springbootdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/analyze")
+public class AnalyzeController {
+
+    @RequestMapping("/carflow")
+    public String car() {
+        return "analyze/carflow";
+    }
+    
+    @RequestMapping("/passengerflow")
+    public String passenger() {
+        return "analyze/passengerflow";
+    }
+
+    @RequestMapping("/ruDianBi")
+    public String rudianbi() {
+        return "analyze/ruDianBi";
+    }
+}
